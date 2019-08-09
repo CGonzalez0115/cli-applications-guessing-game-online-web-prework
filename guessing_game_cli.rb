ce9514804341f68e8f7602f339ce6e3fee17e095
Christina Gonzalez <cmgamway@gmail.com>
2019-08-09 01:37:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,22 @@
 # Code your solution here!
+def run_guessing_game
+end
+  {
+  :game_title => "Guessing Game"
+},
+{
+  if "user clicks play generate and store a random number between 1 and 6"
+    rand (7)
+  when "number generates prompt user to guess a number between 1 and 6"  
+},
+{
+when "user inputs answer compare that to the random number generated"
+},
+
+Capture user input from the command line
+Compare that input to the random number that has been generated
+Print out one of three statements:
+If the user's input matches the random number: You guessed the correct number!
+If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
+It the user's input is equal to "exit": Goodbye!
+Run learn to see your progress and learn submit when you've solved the lab.
